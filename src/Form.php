d8eff0f70ea7cb7f0590956b18dc7cc4062803e2
Vít Kutný <vitkutny@outlook.com>
2014-05-28 14:44:34 +0200

    dev

--- src/Form.php
+++ src/Form.php
@@ -3,7 +3,6 @@
 namespace WebEdit;
 
 use Nette\Application;
-use WebEdit\Form;
 
 final class Form extends Application\UI\Form {
 
@@ -12,24 +11,10 @@ final class Form extends Application\UI\Form {
     public function __construct(Translation\Translator $translator) {
         $this->setTranslator($translator);
         $this->setRenderer(new Form\Renderer);
-        $this->monitor('WebEdit\Form\Control');
     }
 
     public function setEntity($entity) {
         $this->entity = $entity;
-    }
-
-    protected function attached($control) {
-        parent::attached($control);
-        if ($control instanceof Form\Control) {
-            $this->onSubmit[] = [$control, 'handle' . ($this->entity ? 'Edit' : 'Add')];
-            if ($this->entity) {
-                $this->addSubmit('edit', 'form.button.save')->setAttribute('class', 'btn btn-warning');
-                $this->addSubmit('delete', 'form.button.delete')->setValidationScope(FALSE)->setAttribute('class', 'btn btn-danger');
-            } else {
-                $this->addSubmit('add', 'form.button.add');
-            }
-        }
     }
 
     public function getValues($asArray = TRUE) {
