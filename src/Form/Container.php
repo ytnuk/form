6dda9c462dc70b99eca5ca503407356db82724b8
Vít Kutný <vitkutny@outlook.com>
2014-09-25 09:27:44 +0200

    dev

--- src/Form/Container.php
+++ src/Form/Container.php
@@ -3,25 +3,8 @@
 namespace WebEdit\Form;
 
 use Nette\Forms;
-use WebEdit;
-use WebEdit\Form;
 
 abstract class Container extends Forms\Container
 {
 
-    public function __construct()
-    {
-        $this->monitor(Form::class);
-    }
-
-    protected function attached($form)
-    {
-        parent::attached($form);
-        if ($form instanceof Form) {
-            $this->currentGroup = $this->form->currentGroup;
-            $this->configure();
-        }
-    }
-
-    abstract protected function configure();
 }
